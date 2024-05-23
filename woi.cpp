254fb6c8bab0366261ca9186ba9247a46657a365
narges <nargesafzalidv@gmail.com>
2024-05-23 13:51:54 +0330

    making:Constructors for Address class

--- woi.cpp
+++ woi.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 class Address{
     string country, city, street;
+        Address()
+    {
+        country = "United Kingdom";
+        city = "London";
+        street = "Baker";
+    }
+    Address(string countryInput, string cityInput, string streetInput)
+    {
+        country = countryInput;
+        city = cityInput;
+        street = streetInput;
+    }
 };
 int main(){
 
